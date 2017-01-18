e4ce9a66f0d989f8a31714e1e068e0595674f472
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-01-18 08:39:13 -0600

    Refactor NewScream to use addScript
    Instead of dumping these on the page directly, it makes more sense for
    us to use the `addScript` features already exposed by Yesod. This lets
    us use a nice typesafe route, while also centralizing the logic for
    adding these dependencies to the page.
    
    Additionally, since we're now doing extra work and we want to stay
    consistent, we're extracting out a new little helper function for
    rendering the new scream page with a given form and enctype.

--- src/Handler/NewScream.hs
+++ src/Handler/NewScream.hs
@@ -6,6 +6,21 @@ module Handler.NewScream
 import Import
 import Yesod.Markdown (Markdown, markdownField)
 
+getNewScreamR :: Handler Html
+getNewScreamR = do
+    (form, enctype) <- generateFormPost screamForm
+    renderNewScream form enctype
+
+postNewScreamR :: Handler Html
+postNewScreamR = do
+    ((res, form), enctype) <- runFormPost screamForm
+    case res of
+      FormSuccess fields -> do
+          scream <- fromScreamFields fields
+          _screamId <- runDB $ insert scream
+          redirect HomeR
+      _ -> renderNewScream form enctype
+
 data ScreamFields = ScreamFields
     { bodyField :: Markdown
     }
@@ -23,19 +38,10 @@ screamForm = renderDivs $
     ScreamFields
         <$> areq markdownField ("Body" { fsId = Just "scream-body" }) Nothing
 
-getNewScreamR :: Handler Html
-getNewScreamR = do
-    (form, enctype) <- generateFormPost screamForm
-    defaultLayout $ do
-        setTitle "New Scream"
-        $(widgetFile "screams/new")
-
-postNewScreamR :: Handler Html
-postNewScreamR = do
-    ((res, form), enctype) <- runFormPost screamForm
-    case res of
-      FormSuccess fields -> do
-          scream <- fromScreamFields fields
-          _screamId <- runDB $ insert scream
-          redirect HomeR
-      _ -> defaultLayout $(widgetFile "screams/new")
+renderNewScream :: Widget -> Enctype -> Handler Html
+renderNewScream form enctype = defaultLayout $ do
+    setTitle "New Scream"
+    addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"
+    addScript $ StaticR js_markdown_min_js
+    addScript $ StaticR js_char_count_js
+    $(widgetFile "screams/new")
