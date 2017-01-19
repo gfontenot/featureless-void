68fc5e38ddb10e1e62eb4774294011aa74ba557f
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-01-19 07:51:55 -0600

    Paginate screams
    We don't want to display all 18k screams on the root once I
    import those into the database. That would be horrific. Instead, we
    should paginate the root so that it only shows 30 screams at a time.
    
    This is made _dead simple_ using yesod-paginator from the imitable Pat
    Brisbin. My only issue with the lib is that it does a little _too_ much
    for me. I just want to have previous and next links, I don't need the
    ability to jump to an individual page.
    
    So, to accomplish this, I'm going to wrap yesod-paginator in my own
    module that exposes a simpler version of the paginator widget, only
    containing links for Older and Newer screams when appropriate. _Most_ of
    this is taken directly from yesod-paginator so it should be trivial to
    contribute back upstream if it's wanted.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -2,9 +2,17 @@ module Handler.Home where
 
 import Import
 import Helper
+import Yesod.Paginator.Simple
 
 getHomeR :: Handler Html
 getHomeR = do
-    screams <- runDB $ selectList [] [Desc ScreamCreatedAt, Desc ScreamId]
+    (screams, pagination) <- fetchScreams
     defaultLayout $ do
         $(widgetFile "home/index")
+
+fetchScreams :: Handler ([Entity Scream], Widget)
+fetchScreams = runDB $
+    selectSimplePaginated
+        30                                    -- number of items per page
+        []                                    -- filters
+        [Desc ScreamCreatedAt, Desc ScreamId] -- sort descriptors
