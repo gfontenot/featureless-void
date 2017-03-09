12e3e051e9c83e99a510c670bcd9a0b6d6d7a92f
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-03-09 10:27:19 -0600

    Use RepRss for our content type
    If we use `Html` as our content type, the mime type ends up incorrect
    when we serve this route. We'd like it to use an rss mime type instead.
    
    The nicest way to accomplish this is probably to reuse the RepRss
    content type defined by yesod-newsfeed. This type uses the correct
    mimetype and has the required typeclass instances. We could duplicate
    this info ourselves, but since yesod-newsfeed is already a transitive
    dependency, we might as well just use it directly.

--- src/Handler/Feed.hs
+++ src/Handler/Feed.hs
@@ -2,20 +2,21 @@ module Handler.Feed
     ( getFeedR
     ) where
 
+import Yesod.RssFeed (RepRss(..))
 import Text.Hamlet (hamletFile)
 
 import Import
 import Query
 import Helper
 
-getFeedR :: Handler Html
+getFeedR :: Handler RepRss
 getFeedR = do
     screams' <- runDB $ recentScreams
     images <- runDB $ fetchImagesForScreams screams'
     generateFeed $ map (joinOneToMany screamImage images) screams'
 
-generateFeed :: [(Entity Scream, [Entity Image])] -> Handler Html
-generateFeed screams = feedLayout $(widgetFile "feed/main")
+generateFeed :: [(Entity Scream, [Entity Image])] -> Handler RepRss
+generateFeed screams = toRss <$> feedLayout $(widgetFile "feed/main")
 
 feedLayout :: Widget -> Handler Html
 feedLayout widget = do
@@ -25,3 +26,6 @@ feedLayout widget = do
 markupDescription :: Scream -> [Entity Image] -> Widget
 markupDescription scream images =
     $(widgetFile "feed/description")
+
+toRss :: ToContent a => a -> RepRss
+toRss = RepRss . toContent
