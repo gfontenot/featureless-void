e2b7a578ec7ccf6f428523079b1313474aa1558b
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-06-01 15:58:56 -0500

    Add RFC 3339 timestamp formatter
    This is the format used by JSON Feed

--- src/Helper/Time.hs
+++ src/Helper/Time.hs
@@ -1,5 +1,6 @@
 module Helper.Time
-    ( rssTimestamp
+    ( rfc3339Timestamp
+    , rssTimestamp
     , timestamp
     ) where
 
@@ -11,8 +12,14 @@ timestamp = formatTime defaultTimeLocale timestampFormat
 rssTimestamp :: UTCTime -> String
 rssTimestamp = formatTime defaultTimeLocale rssTimestampFormat
 
+rfc3339Timestamp :: UTCTime -> String
+rfc3339Timestamp = formatTime defaultTimeLocale rfc3339TimestampFormat
+
 timestampFormat :: String
 timestampFormat = "%B %d, %Y - %l:%M %p"
 
 rssTimestampFormat :: String
 rssTimestampFormat = "%a, %d %b %Y %T %z"
+
+rfc3339TimestampFormat :: String
+rfc3339TimestampFormat = "%Y-%m-%dT%H:%M:%S%z"
