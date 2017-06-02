8c522668daccca5f9fc5aab52f3a39fa1713a5ac
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-06-02 17:08:19 -0500

    Remove rssTimestamp
    We don't need this anymore, since we're just using the RFC 3339 format
    for feeds.

--- src/Helper/Time.hs
+++ src/Helper/Time.hs
@@ -1,6 +1,5 @@
 module Helper.Time
     ( rfc3339Timestamp
-    , rssTimestamp
     , timestamp
     ) where
 
@@ -9,17 +8,11 @@ import Import
 timestamp :: UTCTime -> String
 timestamp = formatTime defaultTimeLocale timestampFormat
 
-rssTimestamp :: UTCTime -> String
-rssTimestamp = formatTime defaultTimeLocale rssTimestampFormat
-
 rfc3339Timestamp :: UTCTime -> Text
 rfc3339Timestamp = pack . formatTime defaultTimeLocale rfc3339TimestampFormat
 
 timestampFormat :: String
 timestampFormat = "%B %d, %Y - %l:%M %p"
-
-rssTimestampFormat :: String
-rssTimestampFormat = "%a, %d %b %Y %T %z"
 
 rfc3339TimestampFormat :: String
 rfc3339TimestampFormat = "%Y-%m-%dT%H:%M:%S%z"
